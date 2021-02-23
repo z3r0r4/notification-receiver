afcb391da4b1adc212344100b02275f105ce3161
unknown <R4C1.meru@protonmail.com>
2021-02-23 16:19:44 +0100

    working simple toast from console app without callback

--- cantseesharp/NotifiactionReflector/Program.cs
+++ cantseesharp/NotifiactionReflector/Program.cs
@@ -3,6 +3,23 @@ using System.Net.Sockets;
 
 using Microsoft.Toolkit.Uwp.Notifications;
 using Windows.UI.Notifications;
+/*
+At first you need to declare that your program will be using winRT libraries:
+1. Right click on your yourProject, select Unload Project
+2. Right click on your youProject(unavailable) and click Edit yourProject.csproj
+3. Add a new property group:<TargetPlatformVersion>8.0</TargetPlatformVersion>
+4. Reload project
+5. Add referece Windows from Windows > Core
+*/
+using System;
+using Windows.Data.Xml.Dom;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
 
 namespace NotifiactionReflector
 {
@@ -10,23 +27,7 @@ namespace NotifiactionReflector
     {
         static void Main(string[] args)
         {
-
-            // Register AUMID and COM server (for MSIX/sparse package apps, this no-ops)
-            DesktopNotificationManagerCompat.RegisterAumidAndComServer<MyNotificationActivator>("r4.notification-reflector");
-            // Register COM server and activator type
-            DesktopNotificationManagerCompat.RegisterActivator<MyNotificationActivator>();
-
-            // Construct the visuals of the toast (using Notifications library)
-            ToastContent toastContent = new ToastContentBuilder()
-            .AddToastActivationInfo("action=viewConversation&conversationId=5", ToastActivationType.Foreground)
-            .AddText("Hello world!")
-            .GetToastContent();
-
-            // And create the toast notification
-            var toast = new ToastNotification(toastContent.GetXml());
-
-            // And then show it
-            DesktopNotificationManagerCompat.CreateToastNotifier().Show(toast);
+           NewToastNotification Window = new NewToastNotification("Yes",1);
 
             Console.WriteLine("created Toast");
 
@@ -67,4 +68,34 @@ namespace NotifiactionReflector
             }
         }
     }
+     public class NewToastNotification
+    {
+        public NewToastNotification(string input, int type)
+        {
+            string NotificationTextThing = input;
+            string Toast = "";
+            switch (type)
+            {
+                case 1:
+                    {
+                        //Basic Toast
+                        Toast = "<toast><visual><binding template=\"ToastImageAndText01\"><text id = \"1\" >";
+                        Toast += NotificationTextThing;
+                        Toast += "</text></binding></visual></toast>";
+                        break;
+                    }
+                default:
+                    {
+                        Toast = "<toast><visual><binding template=\"ToastImageAndText01\"><text id = \"1\" >";
+                        Toast += "Default Text String";
+                        Toast += "</text></binding></visual></toast>";
+                        break;
+                    }
+            }
+            XmlDocument tileXml = new XmlDocument();
+            tileXml.LoadXml(Toast);
+            var toast = new ToastNotification(tileXml);
+            ToastNotificationManager.CreateToastNotifier("New Toast Thing").Show(toast);
+        }
+}
 }
